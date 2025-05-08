4445459f1b368ae2a90eefc27518947c881ad4f6
lucas1ds <l.alves21@escola.pr.gov.br>
2025-05-08 10:37:37 -0300

    Update lucas 2ds.sql

--- lucas 2ds.sql
+++ lucas 2ds.sql
@@ -9,3 +9,18 @@ especiePlanta varchar (50) not null,
 quantidade int not null,
 tamanho float not null,
 instrucao varchar(150));
+
+create table if not exists canteiros (id bigint auto_increment primary key not null,
+data_plantio date,
+area_plantada varchar(50) not null,
+area_regada varchar(50) not null,
+data_regada date not null,
+quantidade_adubo int,
+adubo varchar(50) not null);
+
+create table if not exists colheitas (id bigint auto_increment primary key not null,
+data_colheita date not null,
+quantidade_colhida varchar(50) not null,
+area_colhida varchar(50) not null,
+qualidade varchar(50),
+destino varchar(50)not null);

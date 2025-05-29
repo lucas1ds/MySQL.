c356bf1d5b1f7885c014c0ad7eb6242cc403c09d
lucas2ds <l.alves21@escola.pr.gov.br>
2025-05-29 08:34:24 -0300

    Update lucasAlves 2DS.sql

--- lucasAlves 2DS.sql
+++ lucasAlves 2DS.sql
@@ -2,21 +2,22 @@ create database dropStep;
 use dropStep;
 
 create table if not exists Marcas( id bigint auto_increment primary key not null,
-marca_id varchar(50) not null,
 nome text not null,
 pais_origem varchar(50),
 ano_fundacao int,
 descricao text);
 
+drop table marcas;
+
 
 create table if not exists Categorias (id bigint auto_increment primary key not null,
-categoria_id varchar(50),
-nome varchar(50),
+nome varchar(50) not null,
 descricao text);
 
 create table if not exists Produtos (id bigint auto_increment primary key not null,
 nome varchar(50),
-categoria_id int,
+categoria_id BIGINT,
+FOREIGN KEY (categoria_id) REFERENCES Categorias(id),
 preco int not null,
 decricao text,
 data_lancamento date,
@@ -26,14 +27,14 @@ tamanhos_disponiveis varchar(50));
 create table if not exists Clientes (id bigint auto_increment primary key not null,
 nome varchar(50) not null,
 email varchar(50),
-telefone int not null,
+telefone varchar(25) not null,
 data_nascimento date not null,
 cpf varchar(50) not null,
 endereco text);
 
 create table if not exists Pedidos( id bigint auto_increment primary key not null,
-pedido_id int,
-cliente_id varchar(50) not null,
+clientes_id BIGINT,
+FOREIGN KEY (clientes_id) REFERENCES Clientes(id),
 data_pedido date,
 valor_total int,
 metodo_pagamento varchar(50),
@@ -46,16 +47,10 @@ tamanho varchar(50),
 quantidade int);
 
 create table if not exists Avaliacoes( id bigint auto_increment primary key not null,
+clientes_id BIGINT,
+FOREIGN KEY (clientes_id) REFERENCES Clientes(id),
 produto_id int,
 cliente_id int,
 nota int,
 comentario text,
 data_avaliacao date);
-
-
-
-
-
-
-
-
